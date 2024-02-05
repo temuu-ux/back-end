acc230ff71a4854ae2a089edc93e195ec89f02d3
Temuulen Batbileg <23LP8360@G401-CC0159.local>
2024-02-05 17:44:15 +0800

    fbjhk

--- src/pages/api/blogjump.js
+++ src/pages/api/blogjump.js
@@ -1,7 +1,7 @@
 export default async function handler(request, response) {
   const res = await fetch("https://dev.to/api/articles?per_page=15&top=5");
-  const blog = await res.json();
+  const blogjump = await res.json();
 
-  response.setHeader("Access-Controll-Allow-Origin", "*");
-  response.status(200).json(blog);
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.status(200).json(blogjump);
 }
